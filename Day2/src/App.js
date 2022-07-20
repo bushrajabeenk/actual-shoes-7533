bae2e48195f4fc789ed6b9225e2b95466c0b74e6
pratikganjale55 <100259765+pratikganjale55@users.noreply.github.com>
2022-07-20 22:17:26 +0530

    day2 final

---
+++ Day2/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Home from './components/home/Home';
+import Navbar from './components/Navbar/Navbar';
+
+
+
+function App() {
+  return (
+  <>
+    <Navbar/>
+    <Home/>
+  </>
+
+  );
+}
+
+export default App;
+
---
+++ Day2/src/components/home/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./home.css"
+
+const Home = () => {
+  return (
+    <>
+    <div className='mainContainer'>
+            <div className='leftDivFinancial'>
+                <div>
+                <h1>Everything you need to<br/>
+                 run your freelance<br/> business.</h1>
+                  <br/>
+                  <h4>Bonsai’s all-in-one product suite with smart <br/> automation lets you focus on your passion, not<br/> your paperwork.
+                  </h4>
+                  <br></br>
+                  <br></br>
+
+                  <input type="text" placeholder='Enter Your email'/>
+                  <button>START FREE</button>
+
+                </div>
+
+            </div>
+            <div className='rightDivFinancial'>
+
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
