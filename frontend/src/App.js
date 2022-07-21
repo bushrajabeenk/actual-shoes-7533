b1586e22f4f24172caa33e85756072d86a295ad2
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-07-22 03:43:51 +0530

    sidebar halfdone

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Mainroute } from './Routes/Mainroute';
+
+function App() {
+  return (
+    <div className="App">
+   <Mainroute/>
+    </div>
+  );
+}
+
+export default App;
