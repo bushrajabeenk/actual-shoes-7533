19324e821715acd8a5aee90a653c46c7540f347f
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-21 21:11:21 +0530

    ch

--- bush/src/components/Store/reducer.js
+++ bush/src/components/Store/reducer.js
@@ -1,7 +1,19 @@
-import { GET_TODOS_LOADING, GET_TODOS_SUCCESS, GET_TODOS_ERROR } from "./types";
+import {
+  GET_TODOS_LOADING,
+  GET_TODOS_SUCCESS,
+  GET_TODOS_ERROR,
+  ADD_TODOS_LOADING,
+  ADD_TODOS_SUCCESS,
+  ADD_TODOS_ERROR,
+} from "./types";
 
 const initState = {
   getTodos: {
+    loading: false,
+    success: false,
+    error: false,
+  },
+  postTodos: {
     loading: false,
     success: false,
     error: false,
@@ -31,6 +43,7 @@ export const todosReducer = (state = initState, { type, payload }) => {
           success: true,
           error: false,
         },
+        todos: payload,
       };
     }
     case GET_TODOS_ERROR: {
@@ -38,6 +51,40 @@ export const todosReducer = (state = initState, { type, payload }) => {
         ...state,
         getTodos: {
           ...state.getTodos,
+          loading: false,
+          success: false,
+          error: true,
+        },
+      };
+    }
+    case ADD_TODOS_LOADING: {
+      return {
+        ...state,
+        postTodos: {
+          ...state.postTodos,
+          loading: true,
+          success: false,
+          error: false,
+        },
+      };
+    }
+    case ADD_TODOS_SUCCESS: {
+      return {
+        ...state,
+        postTodos: {
+          ...state.postTodos,
+          loading: false,
+          success: true,
+          error: false,
+        },
+        todos: [...state.todos, payload],
+      };
+    }
+    case ADD_TODOS_ERROR: {
+      return {
+        ...state,
+        postTodos: {
+          ...state.postTodos,
           loading: false,
           success: false,
           error: true,
--- bush/src/components/Store/types.js
+++ bush/src/components/Store/types.js
@@ -1,3 +1,7 @@
 export const GET_TODOS_LOADING = "get/loading";
 export const GET_TODOS_SUCCESS = "get/success";
 export const GET_TODOS_ERROR = "get/failure";
+
+export const ADD_TODOS_LOADING = "post/loading";
+export const ADD_TODOS_SUCCESS = "post/success";
+export const ADD_TODOS_ERROR = "post/failure";
