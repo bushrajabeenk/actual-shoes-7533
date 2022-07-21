b3f1bc862c7dcaef1b292604be0ff0380af1e2b5
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-21 21:28:08 +0530

    ch

--- bush/src/components/Store/actions.js
+++ bush/src/components/Store/actions.js
@@ -1,5 +1,15 @@
 import axios from "axios";
-import { GET_TODOS_LOADING, GET_TODOS_SUCCESS, GET_TODOS_ERROR } from "./types";
+import {
+  GET_TODOS_LOADING,
+  GET_TODOS_SUCCESS,
+  GET_TODOS_ERROR,
+  ADD_TODOS_LOADING,
+  ADD_TODOS_SUCCESS,
+  ADD_TODOS_ERROR,
+  UPDATE_TODOS_LOADING,
+  UPDATE_TODOS_SUCCESS,
+  UPDATE_TODOS_ERROR,
+} from "./types";
 
 export const getTodosAPI = () => (dispatch) => {
   dispatch({ type: GET_TODOS_LOADING });
@@ -10,5 +20,29 @@ export const getTodosAPI = () => (dispatch) => {
     })
     .catch(() => {
       dispatch({ type: GET_TODOS_ERROR });
+    });
+};
+
+export const addTodoAPI = (payload) => (dispatch) => {
+  dispatch({ type: ADD_TODOS_LOADING });
+  axios
+    .post("", payload)
+    .then((r) => {
+      dispatch({ type: ADD_TODOS_SUCCESS, payload: r.data });
+    })
+    .catch(() => {
+      dispatch({ type: ADD_TODOS_ERROR });
+    });
+};
+
+export const updateTodoAPI = (payload) => (dispatch) => {
+  dispatch({ type: UPDATE_TODOS_LOADING });
+  axios
+    .post("", payload)
+    .then((r) => {
+      dispatch({ type: UPDATE_TODOS_SUCCESS, payload: r.data });
+    })
+    .catch(() => {
+      dispatch({ type: UPDATE_TODOS_ERROR });
     });
 };
