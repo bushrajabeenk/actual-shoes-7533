2a02052c3efea814bb9c6343b574e972a2b9f6f8
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-22 16:39:08 +0530

    ch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import { Mainroute } from './Routes/Mainroute';
+import "./App.css";
+import { Mainroute } from "./Routes/Mainroute";
+import TaskComponent from './components/Tasks/TaskComponent';
 
 function App() {
   return (
     <div className="App">
-   <Mainroute/>
+      <Mainroute />
+      <TaskComponent />
     </div>
   );
 }
---
+++ frontend/src/components/Tasks/TaskComponent.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useRef } from "react";
+import { useState, useEffect } from "react";
+import TaskList from "./TaskList";
+
+const TaskComponent = () => {
+  // const [data, setData] = useState([]);
+  const ref = useRef();
+
+  const dispatch = useDispatch();
+
+  const { getTodos, postTodo, updateTodos, deleteTodo, todos } = useSelector(
+    (state) => state.todo
+  );
+
+  // const addTodo = (text) => {
+  // setData([
+  //   ...data,
+  //   {
+  //     id: data.length + 1,
+  //     text: text,
+  //   },
+  // ]);
+  const addTodo = () => {
+    let value = ref.current.value;
+    dispatch(
+      postTodoAPI({
+        value: value,
+        status: false,
+      })
+    );
+    ref.current.value = null;
+  };
+
+  useEffect(() => {
+    dispatch(getTodosAPI());
+  }, []);
+
+  if (getTodo.loading) return <h1>Loading...</h1>;
+  else if (getTodo.error) return <h1>Something went wrong</h1>;
+  return (
+    <div>
+      <div>
+        <select>
+          <option>Active</option>
+          <option>Completed</option>
+          <option>Archived</option>
+        </select>
+        <select>
+          <option>All Tasks</option>
+          <option>My Tasks</option>
+        </select>
+        <select>
+          <option>All Projects</option>
+        </select>
+      </div>
+      <div>
+        <button>Add From Template</button>
+        <button
+          onClick={() => {
+            addTodo();
+          }}
+        >
+          NEW TASK
+        </button>
+      </div>
+      <div>
+        <TaskList data={data} />
+      </div>
+    </div>
+  );
+};
+
+export default TaskComponent;
