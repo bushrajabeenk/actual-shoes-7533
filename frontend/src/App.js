ab304ae4211a301ce6b9de99bf7eb623de68b468
K Bushra Jabeen <bushjabeen22@gmail.com>
2022-07-22 17:45:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Mainroute } from './Routes/Mainroute';
+import { AllRoutes } from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-   <Mainroute/>
+   <AllRoutes/>
     </div>
   );
 }
