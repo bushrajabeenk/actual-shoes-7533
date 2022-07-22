13283634510f581337ff96f176e620a0ad740cdf
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-22 19:34:06 +0530

    changes

--- frontend/src/components/Store/store.js
+++ frontend/src/components/Store/store.js
@@ -1,4 +1,9 @@
-import { legacy_createStore as createStore } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+  combineReducers,
+} from "redux";
 import thunk from "redux-thunk";
 import { todosReducer } from "./reducer";
 
