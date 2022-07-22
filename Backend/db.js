db7991994a5d2674f19f0f4f44c8592879b433ff
Yash Tomar <99667382+yashtomar15@users.noreply.github.com>
2022-07-22 23:05:47 +0530

    fix files

--- Backend/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-
-const connection=mongoose.connect('mongodb+srv://yashdata15:incorrectPassword@cluster0.xitrb.mongodb.net/Tasks?retryWrites=true&w=majority');
-
-
-
-module.exports={connection};
