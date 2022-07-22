931fad2f56ed7207b7a8814125b8017a9ed7d0a4
yashtomar15 <tomaryash15@gmail.com>
2022-07-22 22:34:43 +0530

    dropdown

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -4,15 +4,21 @@ import { Signup } from "../components/Signup/Signup";
 import { Home } from "../components/Home-afterLogin/Home";
 import { Login } from "../components/Login/Login";
 import TaskComponent from "../components/Tasks/TaskComponent";
+import MainDropdown from '../components/Dropdown/mainDropdown/MainDropdown';
+import Introd from '../components/Introduction/Introd';
+import Navbar from '../components/Navbar/Navbar';
 
 export const AllRoutes = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Navbar />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/tasks" element={<TaskComponent />} />
+        <Route path="/introd" element={<Introd />} />
+        <Route path="/MainDropdown" element={<MainDropdown />} />
       </Routes>
     </div>
   );
