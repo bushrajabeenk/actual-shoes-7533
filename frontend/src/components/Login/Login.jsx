b0a3c8a1bd4e1bb04931f6c8188fb8dc9ebd88ea
K Bushra Jabeen <bushjabeen22@gmail.com>
2022-07-22 17:48:18 +0530

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./login.css"
+import "./Login.css"
 import {  Link, useNavigate } from "react-router-dom";
 import { useState } from 'react';
 import axios from 'axios'
