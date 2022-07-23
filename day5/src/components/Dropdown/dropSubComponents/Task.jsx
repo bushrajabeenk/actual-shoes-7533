a3536de5d254eba6c39eee68ada0f7c47b0c5b30
Pratik Ganjale <100259765+pratikganjale55@users.noreply.github.com>
2022-07-23 23:15:09 +0530

    Add files via upload

---
+++ day5/src/components/Dropdown/dropSubComponents/Task.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Task = () => {
+  return (
+    <div>
+        <div className='upperDivSub'>
+        <h3>Be More Effective</h3>
+        <br/>
+        <p style={{fontSize : "20px"}}>Give your day more structure with<br/> simple task management on your<br/> projects. Track time for each task<br/> and stay up-to-date.</p>
+        <a href='#' >EXPLORE TASKS  <hr/></a>
+
+    </div>
+    <div className='lowerDivSub'>
+        <img src='https://assets-global.website-files.com/58868bcd2ef4daaf0f072900/6050aec11e1747965bfc16a5_task-management-graphic-opt.png' height="600px" width="600px"/>
+
+    </div >
+      
+    </div>
+  )
+}
+
+export default Task
