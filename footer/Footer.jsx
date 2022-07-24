a4c402c6cda78d49cc842b2f8e109f1a841d8480
Pratik Ganjale <100259765+pratikganjale55@users.noreply.github.com>
2022-07-24 15:54:17 +0530

    Add files via upload

---
+++ footer/Footer.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+
+  return (
+    <>
+
+       <div className='footerContainer'>
+            <div className='footerFlexCol'>
+              <b>PRODUCT</b>
+              <br/>
+              <br/>
+                 <ul className="nav flex-column" style={{fontSize : "15px"}}>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Proposals</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Contracts</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Invoicing</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Client CRM</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Task Tracking</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Forms</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Accounting</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Bonsai Tax</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Bonsai Cash</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Bonsai Reviews</a></li>
+                  </ul>
+            </div>
+            <div className='footerFlexCol'>
+              <b>FREE RESOURCES</b>
+              <br/>
+              <br/>
+              <ul className="nav flex-column" style={{fontSize : "15px"}}>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Freelance Resources</a></li>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Freelance Blog by Bonsai</a></li>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">How to Write a Contract</a></li>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Online Signature Maker</a></li>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Self-Employed Taxes Hub</a></li>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Self-Employed Tax Calculator</a></li>
+                    <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Self-Employed Tax Deductions</a></li>
+                </ul>
+                <br/>
+                <br/>
+                <div className='footerFlexCol'>
+              <b>TEMPLATE</b>
+              <br/>
+              <br/>
+              
+              <ul className="nav flex-column" style={{fontSize : "15px"}}>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Invoice Templates</a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Proposal Templates</a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Contract Templates</a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Agreement Templates</a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Scope of Work Templates</a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Quote Templates </a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted"> Credit Note Templates</a></li>
+                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Estimate Templates</a></li>
+               </ul>
+                
+            </div>
+            </div>
+
+            
+            <div className='footerFlexCol'>
+              <b>BONSAI</b>
+              <br/>
+              <br/>
+              
+              <ul className="nav flex-column" style={{fontSize : "15px"}}>
+                         <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Careers</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Support</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">LinkedIn</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Twitter</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Privacy policy</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Legal</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Affiliates</a></li>
+                          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Write for Us</a></li>
+               </ul>
+                
+            </div>
+       </div>
+
+
+    </>
+  )
+}
+
+export default Footer
